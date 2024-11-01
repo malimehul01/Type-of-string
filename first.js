289d0528e6663e76c472e8d47ac691165ffe67e7
mali mehul <malimehul047@gmail.com>
2024-11-01 21:47:06 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,30 @@
+//string
+let string="mali mehul";
+let string2='malimehul';
+
+
+console.log(string.toUpperCase());
+console.log(string2);
+console.log(string[5]);
+
+
+
+
+let obj={
+
+
+   iteam:"pen",
+   price:10,
+
+}
+console.log("this iteam is",obj.iteam,"this price",obj.price,"ruppe");
+//Template Literals
+
+let string3=`mali mehul `;
+console.log(`this iteam is ${obj.iteam} this price ${obj.price} ruppe`);
+console.log(` the calculate ans is ${ 20+30+60+400} `);
+
+console.log(string.replace("mali","raju"));
+
+console.log(string.charAt(4));
+console.log(string+string2);
